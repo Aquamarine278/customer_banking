dd1bc64d6ef9551a3d64b86296069cc797d83f44
Anna Lin <linanna96@gmail.com>
2024-08-20 00:30:12 -0400

    Finished and tested create_cd_file function and customer_banking .py

--- customer_banking.py
+++ customer_banking.py
@@ -18,15 +18,18 @@ def main():
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(updated_savings_balance, interest_earned)
+    print(f'Your updated savings account balance is ${updated_savings_balance: .2f}. Total interest earned is ${interest_earned: .2f}.')
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-
+    cd_balance = input("Please enter your CD account balance: $")
+    cd_interest = input("Please enter the interest rate for your CD account: ")
+    cd_maturity = input("Please enter the number of months since the day of your CD deposit: ")
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f'Your updated savings account balance is ${updated_cd_balance: .2f}. Total interest earned is ${interest_earned: .2f}')
 
 if __name__ == "__main__":
     # Call the main function.
